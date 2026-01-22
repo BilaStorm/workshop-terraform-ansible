a440243a62442afd0d5a9020acb55f973128e943
BilaStorm <clementbarret79@gmail.com>
2026-01-22 16:01:53 +0100

    Update versions.tf

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -12,21 +12,3 @@ terraform {
     }
   }
 }
-# Calcul de variables locales selon l'environnement (workspace)
-locals {
-  # Récupère le workspace actif (dev, prod, ou default)
-  env = terraform.workspace
-
-  # Définit les ports par environnement
-  ports = {
-    default = 8080
-    dev     = 8080
-    prod    = 80
-  }
-
-  # Sélectionne le port correspondant à l'environnement actif
-  nginx_port = local.ports[local.env]
-
-  # Génère un suffixe pour les noms de ressources
-  env_suffix = local.env == "default" ? "" : "-${local.env}"
-}
