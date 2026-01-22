101fabbe5fc1143b846547ade315ec48dfa5241f
BilaStorm <clementbarret79@gmail.com>
2026-01-22 12:03:17 +0100

    Creation main.tf

---
+++ infra/terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Création du réseau (demandé dans les critères)
+resource "docker_network" "private_network" {
+  name = "devops-lab-network"
+}
+
+# Référence à l'image qu'on a construite manuellement à l'étape 1
+resource "docker_image" "flask_app" {
+  name         = "devops-local-lab-flask:latest"
+  keep_locally = true
+}
+
+# Création du conteneur (demandé dans les critères)
+resource "docker_container" "app" {
+  image = docker_image.flask_app.image_id
+  name  = "devops-lab-flask-container"
+  
+  ports {
+    internal = 5000 # Port standard de Flask
+    external = 8080 # Port demandé pour le curl
+  }
+
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+}
